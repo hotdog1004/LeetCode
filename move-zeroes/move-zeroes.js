63c7eb57fe8153d98f9ad6eb3f44499e8c9d375d
gyeol <66353188+hotdog1004@users.noreply.github.com>
2023-04-11 23:51:20 +0900

    Time: 75 ms, Memory: 46.6 MB - LeetHub

---
+++ move-zeroes/move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if(nums.length===1) return nums;
+
+    let idx = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[idx], nums[i]] = [nums[i], nums[idx]]
+            idx++;
+        }
+    }
+};
